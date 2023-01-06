bc8281e19a8ffc866e30f7b1913ef302d21d9af1
DINH MINH THAO <thao.dinh@alobridge.com>
2023-01-06 11:37:38 +0700

    ban test

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const Icone = document.querySelector(".icone");
+const ListNav = document.querySelector(".list-nav");
+
+Icone.addEventListener("click", () => {
+  document.querySelector(".list-nav").classList.toggle("toggle-block");
+  document.querySelector(".header-fix").classList.toggle("height-full");
+  document.querySelector(".icone").classList.toggle("change");
+});
+
+const LocalBtn = document.querySelector(".local");
+
+LocalBtn.addEventListener("click", () => {
+  document.querySelector(".image-map").classList.toggle("d-block");
+  console.log("chay");
+});
